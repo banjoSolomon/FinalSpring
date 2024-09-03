c922ad688504c1d802537d6836c4c28acc2e9f59
Solomon <ayomidebanjo02@gmail.com>
2024-09-03 22:50:10 +0100

    first commit

---
+++ src/test/java/solo/com/FinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
